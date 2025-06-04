6d9a496cf3fcd09fc90700efd7fced4f8c879ae0
Rick-Landa <zs22017010@estudiantes.uv.mx>
2025-06-04 14:41:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { connectToDB } from "./utils/db/dbConnection.js";
 import apiRoutes from "./routes/apiRoutes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const initializeApp = async () => {
   try {
