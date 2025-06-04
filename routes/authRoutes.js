b285bc61681b55664c63248a9f11213872d7ef24
Rick-Landa <zs22017010@estudiantes.uv.mx>
2025-06-04 15:20:02 -0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,10 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/token/access-token', getNewAccessToken);
 
+router.get('/', (req, res) => {
+  res.send('Auth route funcionando');
+});
+
 router.post('/logout', authMiddleware, logout);
 
 export default router;
